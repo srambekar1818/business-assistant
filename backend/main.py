d0e472eda48704b8f9cc45c7ba9cb19fce84231b
Sushil <srambekar2004@gmail.com>
2026-06-27 14:00:20 +0530

    update cors with vercel url

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,9 @@ security = HTTPBearer()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://business-assistant-tau.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
